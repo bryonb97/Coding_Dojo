e19986f97631a0f946e10064f766a740d494e1c2
Bryon Bauer <bryon_bauer@outlook.com>
2019-03-27 14:09:14 -0500

    Disappearing Ninja Assignment

---
+++ CodingDojo/Web_Fundementals/JavaScript/jQuery/Assingnments/DisappearingNinja/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    // alert("ready!");
+
+    $("#img1").click(function(){
+        $(this).hide();
+    });
+
+    $("#img2").click(function(){
+        $(this).hide();
+    });
+
+    $("#img3").click(function(){
+        $(this).hide();
+    });
+
+    $("#img4").click(function(){
+        $(this).hide();
+    });
+
+    $("#img5").click(function(){
+        $(this).hide();
+    });
+
+    $("#img6").click(function(){
+        $(this).hide();
+    });
+
+    $("#img7").click(function(){
+        $(this).hide();
+    });
+
+    $("#img8").click(function(){
+        $(this).hide();
+    });
+
+    $("#restoreBtn").click(function(){
+        $(".image").show();
+    });
+
+});
